c53f7d10e768ecec491be873a31c80b4df7d212f
Khaled <khaledkelany@gmail.com>
2023-05-01 00:18:30 +0300

    frontend MUI and fetch POST and GET methods

---
+++ studentsystemfrontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BasicAppBar from "./components/BasicAppBar"
+import Student from "./components/Student"
+
+function App() {
+  return (
+    <div className="App">
+      <BasicAppBar/>
+      <Student/>
+    </div>
+  );
+}
+
+export default App;
